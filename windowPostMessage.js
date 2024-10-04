7d226f2ad5d330d7648c8277c0072c0641eed70e
Emil Nilsson <emil.nilsson@ductus.se>
2024-10-04 14:56:43 +0200

    Revert to showcase cors error

--- windowPostMessage.js
+++ windowPostMessage.js
@@ -9,28 +9,10 @@ channel = window.opener;
 if (channel) {
   console.log("channel not falsy");
   console.log("channel", channel);
-  // console.log("channel.location", channel.location);
-  // console.log("channel.location.origin", channel.location.origin);
-  // console.log("channel.self", channel.self);
-  // console.log("channel.window", channel.window);
-  // channelRenderer.insertAdjacentText(
-  //   "beforeEnd",
-  //   JSON.stringify(
-  //     Object.entries(channel.location).map(
-  //       (entry) => `${entry[0]}: ${entry[1]}`
-  //     ),
-  //     null,
-  //     2
-  //   )
-  // );
-  // channelRenderer.insertAdjacentText(
-  //   "beforeEnd",
-  //   JSON.stringify(Object.entries(channel.location), null, 2)
-  // );
 
   channel.postMessage(
     `test-message from: netlify`,
-    ALLOWED_ORIGIN
+    channel.location.origin
   );
 } else {
   console.log("channel falsy");
