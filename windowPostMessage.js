3d02666d45371506e5dffd24b7c4651062ec05e7
Emil Nilsson <emil.nilsson@ductus.se>
2024-10-04 14:47:10 +0200

    Comment out channel location due to cors errors

--- windowPostMessage.js
+++ windowPostMessage.js
@@ -9,27 +9,27 @@ channel = window.opener;
 if (channel) {
   console.log("channel not falsy");
   console.log("channel", channel);
-  console.log("channel.location", channel.location);
-  console.log("channel.location.origin", channel.location.origin);
-  console.log("channel.self", channel.self);
-  console.log("channel.window", channel.window);
-  channelRenderer.insertAdjacentText(
-    "beforeEnd",
-    JSON.stringify(
-      Object.entries(channel.location).map(
-        (entry) => `${entry[0]}: ${entry[1]}`
-      ),
-      null,
-      2
-    )
-  );
-  channelRenderer.insertAdjacentText(
-    "beforeEnd",
-    JSON.stringify(Object.entries(channel.location), null, 2)
-  );
+  // console.log("channel.location", channel.location);
+  // console.log("channel.location.origin", channel.location.origin);
+  // console.log("channel.self", channel.self);
+  // console.log("channel.window", channel.window);
+  // channelRenderer.insertAdjacentText(
+  //   "beforeEnd",
+  //   JSON.stringify(
+  //     Object.entries(channel.location).map(
+  //       (entry) => `${entry[0]}: ${entry[1]}`
+  //     ),
+  //     null,
+  //     2
+  //   )
+  // );
+  // channelRenderer.insertAdjacentText(
+  //   "beforeEnd",
+  //   JSON.stringify(Object.entries(channel.location), null, 2)
+  // );
 
   channel.postMessage(
-    `test-message from: ${channel.location}`,
+    `test-message from: netlify`,
     ALLOWED_ORIGIN
   );
 } else {
